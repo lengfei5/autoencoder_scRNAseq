3e24afc3cdf40c38636d8ab035a1b3de05f634eb
jingkui.wang <jingkui.wang@imp.ac.at>
2019-11-04 13:44:58 +0100

    try to fix the matplotlib problem but failed

--- autoencoder_example.py
+++ autoencoder_example.py
@@ -8,14 +8,23 @@ https://blog.keras.io/building-autoencoders-in-keras.html
 
 @author: jingkui.wang
 """
-
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.datasets import mnist
+import numpy as np
 
+#import matplotlib 
+#matplotlib.use('MacOSX')
+import matplotlib.pyplot as plt
+#import sys
+#print (matplotlib.pyplot.get_backend())
+#from IPython.display import display
+#%matplotlib auto
 
 # this is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-
 
 # this is our input placeholder
 input_img = Input(shape=(784,))
@@ -41,13 +50,15 @@ decoder = Model(encoded_input, decoder_layer(encoded_input))
 #First, we'll configure our model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer:
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-
-from keras.datasets import mnist
-import numpy as np
+## prepare the input data using MNIST digits
 (x_train, _), (x_test, _) = mnist.load_data()
 
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
+
+print (x_train.shape)
+print (x_test.shape)
+
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print (x_train.shape)
@@ -55,7 +66,7 @@ print (x_test.shape)
 
 
 autoencoder.fit(x_train, x_train,
-                epochs=10,
+                epochs=1,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test, x_test))
@@ -67,11 +78,12 @@ encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
 
 # use Matplotlib (don't ask)
-import matplotlib.pyplot as plt
+plt.imshow(x_test[0].reshape(28, 28))
+plt.imshow(x_test[2].reshape(28, 28))
 
 n = 10  # how many digits we will display
-
 plt.figure(figsize=(20, 4))
+
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i + 1)
