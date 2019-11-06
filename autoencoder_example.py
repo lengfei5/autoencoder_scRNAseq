1b0293d30fbbf1ef2e51a96a504544ea213a63bf
jingkui.wang <jingkui.wang@imp.ac.at>
2019-11-06 10:25:06 +0100

    testing deep autoencoder example

--- autoencoder_example.py
+++ autoencoder_example.py
@@ -8,6 +8,7 @@ https://blog.keras.io/building-autoencoders-in-keras.html
 
 @author: jingkui.wang
 """
+# special configuration for keras
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import theano
@@ -16,47 +17,16 @@ import keras
 #KERAS_BACKEND=tensorflow python -c "from keras import backend"
 keras.backend.backend()
 
+# import relevant libraries
+import numpy as np
+import matplotlib.pyplot as plt
 from keras.layers import Input, Dense
 from keras.models import Model
-from keras.datasets import mnist
-import numpy as np
 
-#import matplotlib 
-#matplotlib.use('MacOSX')
-import matplotlib.pyplot as plt
-#import sys
-#print (matplotlib.pyplot.get_backend())
-#from IPython.display import display
-#%matplotlib auto
 
-# this is the size of our encoded representations
-encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-
-# this is our input placeholder
-input_img = Input(shape=(784,))
-# "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
-decoded = Dense(784, activation='sigmoid')(encoded)
-
-# this model maps an input to its reconstruction
-autoencoder = Model(input_img, decoded)
-
-# this model maps an input to its encoded representation
-encoder = Model(input_img, encoded)
-
-# create a placeholder for an encoded (32-dimensional) input
-encoded_input = Input(shape=(encoding_dim,))
-# retrieve the last layer of the autoencoder model
-decoder_layer = autoencoder.layers[-1]
-# create the decoder model
-decoder = Model(encoded_input, decoder_layer(encoded_input))
-
-# Now let's train our autoencoder to reconstruct MNIST digits.
-#First, we'll configure our model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer:
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 ## prepare the input data using MNIST digits
+from keras.datasets import mnist
 (x_train, _), (x_test, _) = mnist.load_data()
 
 x_train = x_train.astype('float32') / 255.
@@ -70,9 +40,34 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print (x_train.shape)
 print (x_test.shape)
 
+## start the simple autoencoder with keras
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+# Now let's train our autoencoder to reconstruct MNIST digits.
+#First, we'll configure our model to use a per-pixel binary crossentropy loss, and the Adadelta optimizer:
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 autoencoder.fit(x_train, x_train,
-                epochs=1,
+                epochs=70,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test, x_test))
@@ -109,5 +104,56 @@ plt.show()
 
 
 
+###################
+## deep autoencoder
+###################
+# We do not have to limit ourselves to a single layer as encoder or decoder, we could instead use a stack of layers, such as:
+input_img = Input(shape=(784,))
+encoded = Dense(128, activation='relu')(input_img)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
 
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
 
+autoencoder = Model(input_img, decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(32,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+autoencoder.fit(x_train, x_train,
+                epochs=100,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    
+plt.show()
